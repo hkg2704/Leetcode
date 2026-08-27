252737660e62398d49b6534470499d4524807ce3
Hemanth Kumar Budige <hkg2704@gmail.com>
2026-08-27 22:14:34 +0530

    Time: 2 ms (91.94%) | Memory: 44.7 MB (83.87%) - LeetSync

--- trees/4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
+++ trees/4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
@@ -1,57 +1,46 @@
-import java.util.*;
-
 class Solution {
     public String lexGreaterPermutation(String s, String target) {
         int n = s.length();
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
         }
 
-        // Try to match target up to prefix length 'prefixLen'
-        // and bump character at index 'prefixLen' to something strictly greater.
-        for (int prefixLen = n - 1; prefixLen >= 0; prefixLen--) {
-            int[] counts = freq.clone();
-            boolean possible = true;
+        int matched = 0;
+        while (matched < n) {
+            int c = target.charAt(matched) - 'a';
+            if (count[c] == 0) {
+                break;
+            }
+            count[c]--;
+            matched++;
+        }
 
-            // Try to match target[0 ... prefixLen - 1]
-            for (int j = 0; j < prefixLen; j++) {
-                int idx = target.charAt(j) - 'a';
-                if (counts[idx] > 0) {
-                    counts[idx]--;
-                } else {
-                    possible = false;
-                    break;
-                }
+        for (int pos = Math.min(matched, n - 1); pos >= 0; pos--) {
+            if (pos < matched) {
+                count[target.charAt(pos) - 'a']++;
             }
 
-            if (!possible) continue;
+            int targetChar = target.charAt(pos) - 'a';
 
-            // At position prefixLen, pick the smallest char > target[prefixLen]
-            int targetCharIdx = target.charAt(prefixLen) - 'a';
-            int bumpChar = -1;
-            for (int c = targetCharIdx + 1; c < 26; c++) {
-                if (counts[c] > 0) {
-                    bumpChar = c;
-                    break;
-                }
-            }
+            for (int c = targetChar + 1; c < 26; c++) {
+                if (count[c] > 0) {
+                    count[c]--;
 
-            // If a larger character is available, build the answer
-            if (bumpChar != -1) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(target, 0, prefixLen);
-                sb.append((char) ('a' + bumpChar));
-                counts[bumpChar]--;
+                    StringBuilder result = new StringBuilder();
+                    result.append(target, 0, pos);
+                    result.append((char) ('a' + c));
 
-                // Fill the rest with smallest remaining characters
-                for (int c = 0; c < 26; c++) {
-                    while (counts[c] > 0) {
-                        sb.append((char) ('a' + c));
-                        counts[c]--;
+                    for (int x = 0; x < 26; x++) {
+                        while (count[x] > 0) {
+                            result.append((char) ('a' + x));
+                            count[x]--;
+                        }
                     }
+
+                    return result.toString();
                 }
-                return sb.toString();
             }
         }
 
