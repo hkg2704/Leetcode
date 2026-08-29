1bdf7c8a06cbf03d233248c408f84f9f7d236c96
Hemanth Kumar Budige <hkg2704@gmail.com>
2026-08-29 17:12:39 +0530

    Time: 1 ms (99.98%) | Memory: 42.6 MB (45.62%) - LeetSync

---
+++ trees/7-reverse-integer/reverse-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+    int num=x;
+    long rev=0;
+    while(num!=0)
+    {int rem=num%10;
+        rev=rev*10+rem;
+        num/=10;
+    }
+    if(rev>Integer.MAX_VALUE||rev<Integer.MIN_VALUE)return 0;
+    return (int)rev;
+    }
+}
